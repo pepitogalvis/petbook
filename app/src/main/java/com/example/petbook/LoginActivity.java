0cf8997e9dad897c30e6a1adf639519f14e07589
pepitogalvis <jose.galvis@gmail.com>
2022-10-19 21:48:21 -0500

    Adición de Icono Nuevo de Petbook, traducciones de menu, inicio de sesion exitoso.

--- app/src/main/java/com/example/petbook/LoginActivity.java
+++ app/src/main/java/com/example/petbook/LoginActivity.java
@@ -1,14 +1,209 @@
 package com.example.petbook;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password, name;
+    private Button mlogin;
+    private TextView newdnewaccount, reocverpass;
+    FirebaseUser currentUser;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Crear Cuenta");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // initialising the layout items
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        newdnewaccount = findViewById(R.id.needs_new_account);
+        reocverpass = findViewById(R.id.forgetp);
+        mAuth = FirebaseAuth.getInstance();
+        mlogin = findViewById(R.id.login_button);
+        loadingBar = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        // checking if user is null or not
+        if (mAuth != null) {
+            currentUser = mAuth.getCurrentUser();
+        }
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emaill = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                // if format of email doesn't matches return null
+                if (!Patterns.EMAIL_ADDRESS.matcher(emaill).matches()) {
+                    email.setError("Correo no valido");
+                    email.setFocusable(true);
+
+                } else {
+                    loginUser(emaill, pass);
+                }
+            }
+        });
+
+        // If new account then move to Registration Activity
+        newdnewaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        // Recover Your Password using email
+        reocverpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
+    }
+
+    private void showRecoverPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recuperar Contraseña");
+        LinearLayout linearLayout = new LinearLayout(this);
+        final EditText emailet = new EditText(this);//write your registered email
+        emailet.setText("Email");
+        emailet.setMinEms(16);
+        emailet.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        linearLayout.addView(emailet);
+        linearLayout.setPadding(10, 10, 10, 10);
+        builder.setView(linearLayout);
+        builder.setPositiveButton("Recuperar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String emaill = emailet.getText().toString().trim();
+                beginRecovery(emaill);//send a mail on the mail to recover password
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void beginRecovery(String emaill) {
+        loadingBar.setMessage("Enviado correo de recuperacion....");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        // send reset password email
+        mAuth.sendPasswordResetEmail(emaill).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingBar.dismiss();
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Enviado", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Ocurrio un error", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this, "Error Failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void loginUser(String emaill, String pass) {
+        loadingBar.setMessage("Iniciando Sesión....");
+        loadingBar.show();
+
+        // sign in with email and password after authenticating
+        mAuth.signInWithEmailAndPassword(emaill, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    loadingBar.dismiss();
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    if (task.getResult().getAdditionalUserInfo().isNewUser()) {
+                        String email = user.getEmail();
+                        String uid = user.getUid();
+                        HashMap<Object, String> hashMap = new HashMap<>();
+                        hashMap.put("email", email);
+                        hashMap.put("uid", uid);
+                        hashMap.put("name", "");
+                        hashMap.put("onlineStatus", "online");
+                        hashMap.put("typingTo", "noOne");
+                        hashMap.put("phone", "");
+                        hashMap.put("image", "");
+                        hashMap.put("cover", "");
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                        // store the value in Database in "Users" Node
+                        DatabaseReference reference = database.getReference("Users");
+
+                        // storing the value in Firebase
+                        reference.child(uid).setValue(hashMap);
+                    }
+                    Toast.makeText(LoginActivity.this, "Usuario Registrado " + user.getEmail(), Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(LoginActivity.this, DashboardActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+                } else {
+                    loadingBar.dismiss();
+                    Toast.makeText(LoginActivity.this, "Fallo el inicio de sesión", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this, "Error Occurred", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
